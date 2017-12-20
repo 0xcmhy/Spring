f20ed3221aefd74fbe7224a47ce5c17f36189c13
HXXC\zhangwei <zhangwei@huaxiafinance.com>
2017-12-20 23:41:23 +0800

    TASK:add log

--- spring/spring-mvc/src/main/java/com/cmhy/controller/QueryListAnnotationController.java
+++ spring/spring-mvc/src/main/java/com/cmhy/controller/QueryListAnnotationController.java
@@ -1,26 +1,37 @@
 package com.cmhy.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cmhy.model.BusinessApply;
+
 @Controller
 public class QueryListAnnotationController {
-	@RequestMapping("/queryList")
+	private static final Logger logger = LoggerFactory.getLogger(QueryListAnnotationController.class);
+	@RequestMapping("queryList")
 	public String queryList() {
+		logger.debug("queryList begin: at" + System.currentTimeMillis());
 		BusinessApply businessApply = new BusinessApply();
 		businessApply.setBusinessSum(2000);
 		businessApply.setBusinessType("p2p");
-		return "/index.jsp";
+		logger.debug("queryList end: at" + System.currentTimeMillis());
+		return "index";
 	}
 	@RequestMapping("/queryListJstl")
 	public ModelAndView queryListJstl() {
+		List<BusinessApply>  list = new ArrayList<BusinessApply>();
 		BusinessApply businessApply = new BusinessApply();
 		businessApply.setBusinessSum(2000);
 		businessApply.setBusinessType("p2p");
+		list.add(businessApply);
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("businessApply",businessApply);
+		modelAndView.addObject("name","123");
 		modelAndView.setViewName("index");
 		return modelAndView;
 	}
